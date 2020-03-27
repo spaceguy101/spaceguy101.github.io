24536af9210459665ba1e4f823fa4ffa3d83d0fe
Shreyas Ponkshe <shreyasponkshe12345@gmail.com>
2020-03-27 19:00:34 +0530

    Fix few stuff

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e5e0685e5f6dc1aaa3287883ec8337bd.js"
+  "/precache-manifest.74a62b1ef6b98fc718eac8ea7afbf043.js"
 );
 
 self.addEventListener('message', (event) => {
